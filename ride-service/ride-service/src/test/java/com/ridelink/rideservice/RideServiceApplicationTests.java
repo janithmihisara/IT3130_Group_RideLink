e869a392bf5fbce3ea70b2c87fb668fdc43b824d
Janith.Mihisara <janithmihisara@gmail.com>
2026-09-25 12:51:30 +0530

    chore: initial microservices folder structure setup

---
+++ ride-service/ride-service/src/test/java/com/ridelink/rideservice/RideServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ridelink.rideservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
